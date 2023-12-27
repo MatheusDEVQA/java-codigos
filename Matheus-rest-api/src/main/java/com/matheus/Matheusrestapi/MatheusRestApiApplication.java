9491fd0df7371793ac3bc79532ed08096837ef49
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2023-12-26 22:32:04 -0300

    Create project to practice ci/cd

---
+++ Matheus-rest-api/src/main/java/com/matheus/Matheusrestapi/MatheusRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.Matheusrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatheusRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatheusRestApiApplication.class, args);
+	}
+
+}
---
+++ Matheus-rest-api/src/test/java/com/matheus/Matheusrestapi/MatheusRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.Matheusrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatheusRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
